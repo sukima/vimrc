sukima/vimrc

commit 648b06e1bee8a456f3ef53add0d306dcd217cde7

Add incsearch and gdefault

_vimrc
@@ -111,7 +111,7 @@ if version >= 500
     set backspace=2
     set formatoptions+=nro2lt
     set nohlsearch
-    set noincsearch
+    set incsearch
     set sidescroll=5
     set listchars=eol:$,tab:>~,trail:-,precedes:<,extends:>,nbsp:=
     set showbreak=+
@@ -135,6 +135,7 @@ if version >= 500
     " 'smartcase' turn off ignorecase in a typed search if an uppercase char exists.
     set smartcase
     set infercase
+    set gdefault
     set shortmess=aotTOI
     set shellslash
     set nrformats-=octal

commit 8d74005069929bb6b3ddf94ae508d1bf3694acdb

Fix broken substitutions
This bug was introduced in commit 648b06e1

_vimrc
@@ -506,9 +506,9 @@ noremap <Leader>b :let x=&backup<Bar>set backup<Bar>write<Bar>let &backup=x<Bar>
 " map ;s :up \| saveas! %:p:r-<C-R>=strftime("%y%m%d-%H:%M")<CR>-bak.<C-R>=expand("%:e")<CR> \| 3sleep \| e #<CR>
 " For convinent headers in text files {{{3
 " For underlined header style
-nnoremap <Leader>H1 :t.<Cr>:s/./=/g<Cr>
-nnoremap <Leader>H2 :t.<Cr>:s/./-/g<Cr>
-nnoremap <Leader>H3 :t.<Cr>:s/./~/g<Cr>
+nnoremap <Leader>H1 :t.<Cr>:s/./=/<Cr>
+nnoremap <Leader>H2 :t.<Cr>:s/./-/<Cr>
+nnoremap <Leader>H3 :t.<Cr>:s/./~/<Cr>
 " For octothorp header style
 nnoremap <Leader>h1 :s/^.*$/# & #/<Cr>
 nnoremap <Leader>h2 :s/^.*$/## & ##/<Cr>
@@ -516,7 +516,7 @@ nnoremap <Leader>h3 :s/^.*$/### & ###/<Cr>
 nnoremap <Leader>h4 :s/^.*$/#### & ####/<Cr>
 
 " For convinient markdown table header {{{3
-nnoremap <Leader>T :t.<Cr>:s/[^\|]/-/g<Cr>
+nnoremap <Leader>T :t.<Cr>:s/[^\|]/-/<Cr>
 
 " Quick brackets for functions/if/then/etc deffinitions {{{3
 nnoremap <Leader>) $F(a<Cr><Cr><Up><Esc>"_S